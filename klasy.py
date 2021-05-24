619674b6c85f2fc399ce1a8d99a341a4ac0ac3c5
MrSuhar <56634152+MrSuhar@users.noreply.github.com>
2021-05-24 17:51:25 +0200

    Dodano kilka rzeczy
    Wprowadzanie ilosci produktów
    Wprowadzanie ilosci materiałów
    Wprowadzanie parametrów poszczególnych produktów

---
+++ klasy.py
@@ -0,0 +1,8 @@
+class Info:
+    ilosc_srodkow_produkcji = 0
+    ograniczenia_ilosci_srodkow_produkcji = 0
+
+class Produkt:
+    ilosc_srodkow_produkcji_na_wyrob = []
+    zyski_jednostkowe = 0
+    ograniczenia_ilosci_poszczegolnych_wyrobow = 0
--- main.py
+++ main.py
@@ -1,31 +1,103 @@
 import PySimpleGUI as GUI
+import klasy
 if __name__=="__main__":
 
+    #do okreslenia czy program ma dalej dzialac
+    flag=True
 
-    GUI.theme('DarkAmber')  # Add a touch of color
-    # All the stuff inside your window.
-    layout = [[GUI.Button('boil'), GUI.Button('Exit')]]
+    #OKRESLENIE ILOSCI RODZAJOW SRODKOW PRODUKCJI
+    ilosc_rodzajow_srodkow_produkcji=0
 
-    # Create the Window
-    window = GUI.Window('Window Title', layout)
-    # Event Loop to process "events" and get the "values" of the inputs
+    GUI.theme('DarkAmber')
+    #zawartosc okna
+    layout = [[GUI.InputText(),GUI.Button('Dodaj ilosc rodzajow materialow')], [GUI.Button('Exit')]]
+
+    #tworzenie okna
+    window = GUI.Window('Ilosc rodzajow materialow', layout)
+
     while True:
         event, values = window.read()
         if event == GUI.WIN_CLOSED or event == 'Exit':  # if user closes window or clicks cancel
+            flag=False
             break
-        if event =='boil':
-            print("Welcome to BOiL")
+        if event =='Dodaj ilosc rodzajow materialow':
 
+            if values[0]!='' and int(values[0])>0:
+                ilosc_rodzajow_srodkow_produkcji=int(values[0])
+                break
+            else:
+                continue
     window.close()
 
+    # print(ilosc_rodzajow_srodkow_produkcji)
+
+    # OKRESLENIE ILOSCI RODZAJOW PRODUKTOW
+    ilosc_rodzajow_produktow = 0
+    if flag:
+        layout = [[GUI.InputText(),GUI.Button('Dodaj ilosc rodzajow produktow')], [GUI.Button('Exit')]]
+
+        window = GUI.Window('Ilosc rodzajow produktow', layout)
+        while True:
+            event, values = window.read()
+            if event == GUI.WIN_CLOSED or event == 'Exit':  # if user closes window or clicks cancel
+                flag=False
+                break
+            if event == 'Dodaj ilosc rodzajow produktow':
+                if values[0] != '' and int(values[0]) > 0:
+                    ilosc_rodzajow_produktow = int(values[0])
+                    break
+                else:
+                    continue
+        window.close()
+
+    #print(ilosc_rodzajow_produktow)
 
 
-class Info:
-    ilosc_srodkow_produkcji = 0
-    ograniczenia_ilosci_srodkow_produkcji = 0
+    #DODAWANIE ILOSCI MATERIALOW POTRZEBNYCH DO WYPRODUKOWANIA KONKRETNEGO PRODUKTU
 
-class Produkt:
-    ilosc_srodkow_produkcji_na_wyrob = 0
-    zyski_jednostkowe = 0
-    ograniczenia_ilosci_poszczegolnych_wyrobow = 0
+    #WORK IN PROGRESS-> dodac petle przechodzaca przez wszystkie produkty
+    testowy_produkt=klasy.Produkt()
+    if flag:
+        nr_produktu='test'
+        layout = [[GUI.Text('Zysk jednostkowy: '),GUI.InputText()],
+                  [GUI.Text('Ograniczenie ilosci: '),GUI.InputText()]
+            ,*[[GUI.Text("Srodek produkcji nr "+str(i+1)),GUI.InputText(), ] for i in range(ilosc_rodzajow_srodkow_produkcji)],
+                   [GUI.Button('Zapisz'),GUI.Button('Exit')]]
+
+        window=GUI.Window('Produkt '+str(nr_produktu), layout)
+        while True:
+            event, values = window.read()
+            if event == GUI.WIN_CLOSED or event == 'Exit':
+                flag=False
+                break
+            if event == 'Zapisz':
+                #Dodawanie zyskow jednostkowych && ograniczenia ilosci
+                if values[0] != '':
+                    testowy_produkt.zyski_jednostkowe=int(values[0])
+                if values[1] != '' and int(values[1])>=0:
+                    testowy_produkt.ograniczenia_ilosci_poszczegolnych_wyrobow=int(values[1])
+
+                #Dodawanie ilosci srodkow produkcji potrzebnych do wyproukowania 1 szt. danego wyrobu
+                for i in range(len(values)):
+                    #print(values[i])
+                    if i>1:
+                        if values[i]!='' and int(values[i])>=0:
+                            testowy_produkt.ilosc_srodkow_produkcji_na_wyrob.append(int(values[i]))
+        window.close()
+
+        print("Zyski: "+ str(testowy_produkt.zyski_jednostkowe))
+        print("Ograniczenie ilosci: " + str(testowy_produkt.ograniczenia_ilosci_poszczegolnych_wyrobow))
+        print("Zyski: " + str(testowy_produkt.ilosc_srodkow_produkcji_na_wyrob))
+
+
+
+
+
+
+
+
+    #mana=klasy.Info()
+    #mana.ilosc_srodkow_produkcji=11
+    #print(mana.ilosc_srodkow_produkcji)
+
 
