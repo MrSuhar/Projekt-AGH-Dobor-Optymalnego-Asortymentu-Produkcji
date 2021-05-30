53183d09c56ba5ad31f09d5e28f8bdacd308fd5c
KamilaNowak <knowak242@gmail.com>
2021-05-30 13:58:05 +0200

    clean & fix

--- klasy.py
+++ klasy.py
@@ -1,7 +1,6 @@
 from scipy.optimize import linprog
 import numpy as np
 
-
 class Info:
     ilosc_srodkow_produkcji = 0
     ograniczenia_ilosci_srodkow_produkcji = 0
@@ -15,27 +14,43 @@ class Produkt:
 
 
 def optymalizuj(zyski, czas_pracy, limit_pieca, ograniczenia):
+
+    print("Wczytano:")
+    print("Zyski: \t", zyski)
+    print("Czas pracy: \t",czas_pracy)
+    print("Limit pracy pieca: \t ",limit_pieca)
+    print("Ograniczenia: \t",ograniczenia)
+    print("----------------------------")
+
     zyski = zyski * (-1.0)
     srodki_produkcji = []
     zysk_calkowity = 0
 
-    wynik = linprog(c=zyski, A_ub=czas_pracy, b_ub=limit_pieca, bounds=ograniczenia, method="simplex")
-
     # linprog.x -The independent variable vector which optimizes the linear programming problem.
-    if wynik.success:
-        wynik.x = list(map(lambda x: round(x, 0), wynik.x))
+    try:
+        wynik = linprog(c=zyski,
+                        A_ub=czas_pracy,
+                        b_ub=limit_pieca,
+                        bounds=ograniczenia,
+                        method="simplex")
 
-        ilosc_wyrobow = wynik.x
-        zyski = zyski * (-1.0)
+        if wynik.success:
+            wynik.x = list(map(lambda x: round(x, 0), wynik.x))
 
-        for row in czas_pracy:
-            srodki_produkcji.append(sum(wynik.x * row))
+            ilosc_wyrobow = wynik.x
+            zyski = zyski * (-1.0)
 
-        zysk_calkowity = (wynik.fun * (-1))
+            for row in czas_pracy:
+                srodki_produkcji.append(sum(wynik.x * row))
 
-        info = Info()
-        info.ilosc_srodkow_produkcji = srodki_produkcji
-        info.ograniczenia_ilosci_srodkow_produkcji = ilosc_wyrobow
-        info.zysk_calkowity = zysk_calkowity
+            zysk_calkowity = (wynik.fun * (-1))
 
-        return info
+            info = Info()
+            info.ilosc_srodkow_produkcji = srodki_produkcji
+            info.ograniczenia_ilosci_srodkow_produkcji = ilosc_wyrobow
+            info.zysk_calkowity = zysk_calkowity
+
+            return info
+    except:
+        print("Nie można zoptymalizować")
+        return 1
